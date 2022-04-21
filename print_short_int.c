963b21345a6202ebfda22cf4a0b6bfb9f952425a
marceesty <marceesty@gmail.com>
2022-04-21 10:24:44 +0100

    printf

--- print_short_int.c
+++ print_short_int.c
@@ -1,47 +1,41 @@
 #include "main.h"
 
 /**
- * prinhhex - prints a short decimal in hexadecimal
+ * prinhint - prints a short integer
  * @arguments: input string
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
- * Return: number of chars printed
+ * Return: number of chars printed.
  */
 
-int prinhhex(va_list arguments, char *buf, unsigned int ibuf)
+int prinhint(va_list arguments, char *buf, unsigned int ibuf)
 {
-short int int_input, i, isnegative, count, first_digit;
-char *hexadecimal, *binary;
+short int int_input;
+unsigned short int int_in, int_temp, i, div, isneg;
 
 int_input = va_arg(arguments, int);
-isnegative = 0;
-if (int_input == 0)
-{
-ibuf = handl_buf(buf, '0', ibuf);
-return (1);
-}
+isneg = 0;
 if (int_input < 0)
 {
-int_input = (int_input * -1) - 1;
-isnegative = 1;
+int_in = int_input * -1;
+ibuf = handl_buf(buf, '-', ibuf);
+isneg = 1;
 }
-
-binary = malloc(sizeof(char) * (16 + 1));
-binary = fill_binary_array(binary, int_input, isnegative, 16);
-hexadecimal = malloc(sizeof(char) * (4 + 1));
-hexadecimal = fill_hex_array(binary, hexadecimal, 0, 4);
-for (first_digit = i = count = 0; hexadecimal[i]; i++)
+else
 {
-if (hexadecimal[i] != '0' && first_digit == 0)
-first_digit = 1;
-if (first_digit)
+int_in = int_input;
+}
+int_temp = int_in;
+div = 1;
+while (int_temp > 9)
 {
-ibuf = handl_buf(buf, hexadecimal[i], ibuf);
-count++;
+div *= 10;
+int_temp /= 10;
 }
+for (i = 0; div > 0; div /= 10, i++)
+{
+ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
 }
-free(binary);
-free(hexadecimal);
-return (count);
+return (i + isneg);
 }
 
